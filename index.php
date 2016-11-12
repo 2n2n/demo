d98aa3151d21298340ec98df2fb70130e514b264
Anthony Yolach Lloveras <tontonquit@gmail.com>
2016-11-12 15:05:56 +0000

    commit with bug

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ function sum($num1, $num2) {
     return $num1 + $num2;
 }
 
-echo "the sum is:" .sum(1,1);
+function subract($num1, $num2)
+{
+    return $num1 - $num2;
+}
 
+echo "the sum is:" .sum(1,1);
+echo "the diff is: ". subtract(1,1);
 echo "success!";
