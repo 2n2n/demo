33cda1ea1ff99b09a18de24020b01895b162386d
Anthony Yolach Lloveras <tontonquit@gmail.com>
2016-11-12 15:08:46 +0000

    added more complex code.

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ function sum($num1, $num2) {
     return $num1 + $num2;
 }
 
-function subract($num1, $num2)
+function subtract($num1, $num2)
 {
     return $num1 - $num2;
 }
 
 echo "the sum is:" .sum(1,1);
-echo "the diff is: ". subtract(1,1);
+echo "<br/>";
+echo "the diff is: ". subtract(1,sum(1,1));
+echo "<br/>";
 echo "success!";
